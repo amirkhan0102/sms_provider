ae054db1baf31958ea3f01f3b2b0b4235dab49ca
amirkhan0102 <amriddinjavqochdiyev@gmail.com>
2026-06-01 14:49:10 +0500

    project added

---
+++ smsprovider/src/main/java/dasturlash/uz/smsprovider/SmsproviderApplication.java
@@ -0,0 +1,13 @@
+package dasturlash.uz.smsprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsproviderApplication.class, args);
+	}
+
+}
---
+++ smsprovider/src/test/java/dasturlash/uz/smsprovider/SmsproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package dasturlash.uz.smsprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
